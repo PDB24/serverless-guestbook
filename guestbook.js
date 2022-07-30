1e287316995178620c20916648ad329cc2e0204f
PDB24 <108351340+PDB24@users.noreply.github.com>
2022-07-30 14:46:31 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://0e0efed6.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
